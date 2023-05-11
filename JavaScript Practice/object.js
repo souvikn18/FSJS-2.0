c3843ca3b210213c612751a8f2d659f207c38281
souvikn18 <souvikn20@gmail.com>
2023-05-11 12:23:30 +0530

    Learning object static methods

--- JavaScript Practice/object.js
+++ JavaScript Practice/object.js
@@ -84,3 +84,110 @@ const obj5 = {
 
 //object chaining
 console.log(obj5.printcomic().printcomic().printcomic().printcomic());
+
+// console.log(Object.prototype);
+
+// const obj = {
+//     foo: 1,
+//     // You should not define such a method on your own object,
+//     // but you may not be able to prevent it from happening if
+//     // you are receiving the object from external input
+//     propertyIsEnumerable() {
+//       return false;
+//     },
+//   };
+  
+//   console.log(obj.propertyIsEnumerable("foo"));  // false; unexpected result
+//  console.log(Object.prototype.propertyIsEnumerable.call(obj, "foo"));  // true; expected result
+
+const obj6 = { 
+    a: 1,
+    b:2
+};
+// let x= toString(obj6);
+// for(x in obj6){
+//     console.log(x);
+// }
+// console.log(obj6.valueOf());
+
+const ages = { alice: 18, bob: 27 };
+
+function hasPerson(name) {
+  return name in ages;
+}
+
+function getAge(name) {
+  return ages[name];
+}
+
+// console.log(hasPerson("hasOwnProperty"));  // true
+// console.log(getAge("toString"));  // [Function: toString]
+let a = hasPerson('hasOwnProperty');
+// console.log(a);
+let b = getAge('toString');
+// console.log(b);
+
+//Object.assign()       // copies all enumerable own properties from one or more source objects to a target object. It returns the modified target object.
+const target = { a: 1, b: 2 };
+const source = { b: 4, c: 5 };
+const onemore = {b:6, c:10, d:20}
+
+const returnedTarget = Object.assign(target, source,onemore);
+
+// console.log(target);
+// Expected output: Object { a: 1, b: 4, c: 5 }
+
+// console.log(returnedTarget === target);
+// Expected output: true
+
+//Object.create     //creates a new object, using an existing object as the prototype of the newly created object
+
+const person = {
+    isHuman: false,
+    printIntroduction: function(){
+        console.log(`My name is ${this.name}. Am I human? ${this.isHuman}`);
+    }
+}
+// const me = {
+//     name: 'souvik',
+//     isHuman: true
+// }
+const me = Object.create(person);
+
+me.name = 'Souvik'; // "name" is a property set on "me", but not on "person"
+me.isHuman = true; // Inherited properties can be overwritten
+
+// me.printIntroduction();
+// console.log(person);
+// console.log(me);
+
+//Object.properties()
+
+const object1 = {};
+
+Object.defineProperties(object1, {
+  property1: {
+    value: 42,
+    writable: true
+  },
+  property2: {},
+  property3:{
+   value:50,
+   writable:true
+  },
+});
+
+// console.log(object1.property3);
+const obj7 = {};
+Object.defineProperties(obj7, {
+  property1: {
+    value: true,
+    writable: true,
+  },
+  property2: {
+    value: "Hello",
+    writable: false,
+  },
+  // etc. etc.
+});
+// console.log(obj7.property1);
